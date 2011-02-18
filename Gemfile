2493a03ef89ea09c8c2458afc97806dded71816d
Austin Schneider <soccer022483@gmail.com>
2011-02-18 15:52:33 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+# gem 'pg'
+
+group :test do
+  gem 'capybara', '0.4.1.2'
+  gem 'rspec-rails', '2.5.0'
+  gem 'cucumber-rails', '0.3.2'
+  gem 'autotest', '4.4.6'
+  gem 'autotest-rails', '4.1.0'
+  gem 'factory_girl_rails', '1.1.beta1'
+  gem 'database_cleaner', '0.6.3'
+  gem 'launchy', '0.3.7'
+  gem 'webmock', '1.6.2'
+  gem 'cover_me', '1.0.0.rc5'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Genesis::Application.config.secret_token = '5025b7de5db135d352df84c4459e11647f4f5c038a9f6b924d4d37261a5c3188df677204ef753da5c3271216c73c8f09ae4780762ce601d23a661e223924f3bd'
